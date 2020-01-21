39982249562557755100fce68444e60e0604a31f
EL-ABBADI Soufiane <elabbadisoufiane@gmail.com>
2020-01-21 19:38:02 +0100

    Updates

---
+++ precache-manifest.1c38355252aaa64938d11b3183fb9edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaa55d6b5a5834e2f5f26137e648ee9",
+    "url": "/plp/index.html"
+  },
+  {
+    "revision": "5b740bb105e77a134440",
+    "url": "/plp/static/css/main.ce536280.chunk.css"
+  },
+  {
+    "revision": "a7a838fdd5420fbc0c7e",
+    "url": "/plp/static/js/2.d6f55002.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/plp/static/js/2.d6f55002.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b740bb105e77a134440",
+    "url": "/plp/static/js/main.c76eae90.chunk.js"
+  },
+  {
+    "revision": "838af049d903a0a9937a",
+    "url": "/plp/static/js/runtime-main.f558bb59.js"
+  }
+]);
